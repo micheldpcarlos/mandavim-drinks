d1d1bce36e9aa43943ab70fdc08f30e44597bef9
Michel Carlos <mcarlos@littleotterhealth.com>
2023-06-16 22:44:22 -0300

    add some desktop styles

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,9 @@ import "@ionic/vue/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
+/* Simple desktop theme */
+import "./theme/desktop.css";
+
 // firebase shit
 import { VueFire, VueFireAuth } from "vuefire";
 import { firebaseApp } from "./firebase";
